97bd2304dee571c225de012ba93d571fc64b5de0
Guahoo <ilylyubov@gmail.com>
2019-12-28 15:01:53 +0300

    gjkh

--- app/src/main/java/com/app/simpleweather/Utility/OftenUsedStrings.java
+++ app/src/main/java/com/app/simpleweather/Utility/OftenUsedStrings.java
@@ -1,5 +1,28 @@
 package com.app.simpleweather.Utility;
 
 public enum OftenUsedStrings {
+    OPENSAGEDATA_API("835b77b309444de689cd7c07b675493e"),
+    RESULTS("results"),
 
+
+    CITY("city"),
+    VILLAGE("village"),
+    HAMLET("hamlet"),
+    FORMATTED("formatted"),
+    COMPONENTS("components"),
+    COMMA(","),
+    LOCATION("Ваше местоположение"),
+    NO_SIGNAL("Нет сети"),
+    CITY_NAME("city_name"),
+    COUNTY("county");
+
+    private String oftenUsedString;
+
+    OftenUsedStrings(String s) {
+        this.oftenUsedString = s;
+    }
+
+    public String getOftenUsedString() {
+        return oftenUsedString;
+    }
 }
